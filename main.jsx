2f20d8a1ca59c386fea1009348b8c5ae5f24ccd1
exyt-admin <admin@myexyt.com>
2026-05-13 08:52:28 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../exyt_training_recommender.jsx'
+import App from './exyt_training_recommender.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
