09f1e06b1bf950902f3ae9ad139c79cde96fedbe
exyt-admin <admin@myexyt.com>
2026-05-13 08:45:42 +0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../exyt_training_recommender.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
